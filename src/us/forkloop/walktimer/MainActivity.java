74739ebde320ae48ab5882064c8036c3ed1753cc
forkloop <forkloop@gmail.com>
2012-10-30 16:14:59 -0400

    Initial commit.

---
+++ src/us/forkloop/walktimer/MainActivity.java
@@ -0,0 +1,142 @@
+package us.forkloop.walktimer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity {
+
+    private final static long DISTANCE = 50;
+    private final static String DEFAULT_STRING = "0 s";
+    private final static int TOTAL_ADJUST = 10;
+    private final static double THRESHOLD = 1.0;
+    private static boolean isUpdate = true;
+
+    private TextView timeView;
+    private EditText editText;
+    private long startTime;
+    private double aimingDistance;
+    private Location previousLocation;
+    private int adjustCount;
+    private Location startLocation;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        editText = (EditText) findViewById(R.id.distance);
+        timeView = (TextView) findViewById(R.id.time);
+        
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new XLocationListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        locationManager.removeUpdates(locationListener);
+        timeView.setText(DEFAULT_STRING);
+        super.onPause();
+    }
+
+    public void onToggleClicked(View view) {
+        boolean isOn = ((ToggleButton) view).isChecked();
+        if (isOn) {
+            adjustCount = 0;
+            isUpdate = true;
+            try {
+                aimingDistance = Double.parseDouble(editText.getText().toString());
+                if (aimingDistance < 0) {
+                    Toast.makeText(getApplicationContext(), "Distance must be positive!", Toast.LENGTH_SHORT).show();
+                    ((ToggleButton) view).setChecked(false);
+                    Log.i(this.getClass().getSimpleName(), "Invalid distance: " + aimingDistance);
+                    return ;
+                }
+            } catch (NumberFormatException nfe) {
+                Log.w(this.getClass().getSimpleName(), nfe);
+                aimingDistance = DISTANCE;
+            }
+            Log.i(this.getClass().getSimpleName(), "Start to timing distance: " + aimingDistance);
+            timeView.setText("Waiting for GPS...");
+            previousLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (previousLocation != null) {
+                Log.i(this.getClass().getSimpleName(), "latitude: " + previousLocation.getLatitude() + ", longitude: " + previousLocation.getLongitude());
+            } else {
+                previousLocation = new Location(LocationManager.GPS_PROVIDER);
+            }
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } else {
+            locationManager.removeUpdates(locationListener);
+            timeView.setText(DEFAULT_STRING);
+        }
+    }
+
+    private class XLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i(this.getClass().getSimpleName(), "latitude: " + location.getLatitude() + ", longitude: " + location.getLongitude());
+            if (isUpdate) {
+                double distance = location.distanceTo(previousLocation);
+                Log.i(this.getClass().getSimpleName(), "adjust distance: " + distance);
+                if (distance < THRESHOLD) {
+                    adjustCount++;
+                } else {
+                    adjustCount = 0;
+                }
+                Log.i(this.getClass().getSimpleName(), "adjustCount: " + adjustCount);
+                if (adjustCount == TOTAL_ADJUST) {
+                    isUpdate = false;
+                    startLocation = location;
+                    Log.i(this.getClass().getSimpleName(), "Start at: " + startLocation.getLatitude() + " : " + startLocation.getLongitude());
+                    timeView.setText("Starting...");
+                    startTime = System.currentTimeMillis();
+                }
+                previousLocation = location;
+            } else {
+                double distance = location.distanceTo(startLocation);
+                timeView.setText(String.format("%.1f m", distance));
+                if (distance >= aimingDistance) {
+                    long endTime = System.currentTimeMillis();
+                    double totalTime = (endTime - startTime)/1000.0;
+                    timeView.setText(totalTime + " s");
+                    locationManager.removeUpdates(locationListener);
+                }
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.w(this.getClass().getSimpleName(), provider + " is disabled...");
+            Toast.makeText(getApplicationContext(), "Please enable " + provider, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.i(this.getClass().getSimpleName(), provider + " status: " + status);
+            Log.i(this.getClass().getSimpleName(), "Number of satellites: " + extras.getInt("satellites"));
+        }
+    }
+}
